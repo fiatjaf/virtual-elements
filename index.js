72900ddcb18e1645bb80382d0793828634b6a96e
fiatjaf@spooner <fiatjaf@gmail.com>
2015-01-04 10:19:03 -0500

    convert numbers to strings to avoid exceptions

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ var h = require('virtual-hyperscript')
 
 var tags = ['a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdi', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'dir', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'keygen', 'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'menu', 'menuitem', 'meta', 'meter', 'nav', 'noframes', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'tt', 'u', 'ul', 'var', 'video', 'wbr']
 
-module.exports = {}
-var _slice = [].slice
-
 var createElement = module.exports.createElement = function (tagName) {
   return function () {
-    return h(tagName, arguments[0], _slice.call(arguments, 1))
+    try {
+      return h(tagName, arguments[0], [].slice.call(arguments, 1))
+    } catch (e) {
+      if (e.foreignObject && typeof e.foreignObject == 'number') {
+        return h(tagName, arguments[0], '' + arguments[1])
+      }
+    }
   }
 }
 
